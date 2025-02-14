c7b5e10cf8d7ca6171ab4fb6a3504de136087d32
Gabriel Ribeiro Pinto <123651459+GabrielNeoX3@users.noreply.github.com>
2025-02-14 17:34:46 +0000

    Adiciona a página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Olá, eu tenho uma mensagem para você: 90% do sucesso se baseia em insistir. Você tem tudo que precisa para construir algo grandioso!</h1>
+}
+
+export default Home;
